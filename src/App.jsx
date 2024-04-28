906329c6d60df4fbb2aa0e1d79d77ce99553aa7c
TatoNaranjo <naranjosa2004@gmail.com>
2024-04-28 12:40:00 -0500

    Update width of container in App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Detail from "./components/detail/Detail"
 
 const App = () => {
   return (
-    <div className='container flex backdrop-blur bg-lime-950/50 saturate-[180%] w-[90vw] h-[90vh] rounded-xl text-white font-bold '>
+    <div className='container flex backdrop-blur bg-lime-950/50 saturate-[180%] w-[80vw] h-[90vh] rounded-xl text-white font-bold '>
       <List/>
       <Chat/>
       <Detail/>
