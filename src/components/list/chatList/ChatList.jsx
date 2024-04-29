b8f00940b41cae820aa7e02ea83ebd2673bd8dd5
TatoNaranjo <naranjosa2004@gmail.com>
2024-04-28 22:53:13 -0500

    Add useEffect and handleSelect functions to ChatList component

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,10 +1,73 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./chatList.css";
 import AddUser from "./addUser/AddUser";
+import { useUserStore } from "../../../lib/userStore";
+import { doc, getDoc, onSnapshot, updateDoc } from "firebase/firestore";
+import { db } from "../../../lib/firebase";
+import { useChatStore } from "../../../lib/chatStore";
 
 const chatList = () => {
+  const [input, setInput] = useState("");
   const [addMode, setAddMode] = useState(false);
-  
+  const [chats, setChats] = useState([]);
+
+  const { currentUser } = useUserStore();
+  const { chatId, changeChat } = useChatStore();
+
+  useEffect(() => {
+    const unSub = onSnapshot(
+      doc(db, "userchats", currentUser.id),
+      async (res) => {
+        const items = res.data().chats;
+
+        const promises = items.map(async (item) => {
+          const userDocRef = doc(db, "users", item.receiverId);
+          const userDocSnap = await getDoc(userDocRef);
+
+          const user = userDocSnap.data();
+
+          return { ...item, user };
+        });
+
+        const chatData = await Promise.all(promises);
+        setChats(chatData.sort((a, b) => b.updatedAt - a.updatedAt));
+        setAddMode(false);
+      }
+    );
+
+    return () => {
+      unSub();
+    };
+  }, [currentUser.id]);
+
+  const handleSelect = async (chat) => {
+    const userChats = chats.map((item) => {
+      const { user, ...rest } = item;
+
+      return rest;
+    });
+
+    const chatIndex = userChats.findIndex(
+      (item) => item.chatId === chat.chatId
+    );
+
+    userChats[chatIndex].isSeen = true;
+
+    const userChatsRef = doc(db, "userchats", currentUser.id);
+    
+    try {
+      await updateDoc(userChatsRef, {
+        chats: userChats,
+      });
+      changeChat(chat.chatId, chat.user);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+
+  const filteredChats = chats.filter((c) => c.user.username.toLowerCase().includes(input.toLowerCase())
+  )
   return (
     <div className="chatList flex-[1] overflow-scroll">
       <div className="search flex items-center gap-5 p-5">
@@ -14,6 +77,7 @@ const chatList = () => {
             className="bg-transparent text-white border-none outline-none flex-[1]"
             type="text"
             placeholder="search"
+            onChange={(e) => setInput(e.target.value)}
           />
         </div>
         <img
@@ -23,37 +87,38 @@ const chatList = () => {
           onClick={() => setAddMode((prev) => !prev)}
         />
       </div>
-
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Hikaru Nakamura</span>
-          <p>I'm Out Now</p>
+      {filteredChats.map((chat) => (
+        <div
+          className="item"
+          key={chat.chatId}
+          onClick={() => handleSelect(chat)}
+          style={{
+            backgroundColor: chat?.isSeen
+              ? "transparent"
+              : "rgba(17,25,40,0.9)",
+          }}
+        >
+          <img
+            src={
+              chat.user.blocked.includes(currentUser.id)
+                ? "./avatar.png"
+                : chat.user.avatar || "./avatar.png"
+            }
+            alt=""
+          />
+          <div className="texts">
+            <span>
+              {chat.user.blocked.includes(currentUser.id)
+                ? "User"
+                : chat.user.username}
+            </span>
+            <p>{chat.lastMessage}</p>
+          </div>
         </div>
-      </div>
+      ))}
 
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Hikaru Nakamura</span>
-          <p>I'm Out Now</p>
-        </div>
-      </div>
-
-
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Hikaru Nakamura</span>
-          <p>I'm Out Now</p>
-        </div>
-      </div>
-
-      {addMode && <AddUser/>}
+      {addMode && <AddUser />}
     </div>
-
-
-    
   );
 };
 
