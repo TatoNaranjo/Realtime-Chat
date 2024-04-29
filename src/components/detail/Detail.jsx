6d7f263117ae891cccdb9bb48c46f2b80ca90c47
TatoNaranjo <naranjosa2004@gmail.com>
2024-04-28 22:52:53 -0500

    Add block user functionality and logout button

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,11 +1,36 @@
+import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { useChatStore } from "../../lib/chatStore";
+import { auth, db } from "../../lib/firebase";
+import { useUserStore } from "../../lib/userStore";
 import "./detail.css"
 
 const Detail = () => {
+
+    const {chatId, user, isCurrentUserBlocked, isReceiverBlocked, changeBlock} = useChatStore();
+
+    const {currentUser} = useUserStore();
+    const handleBlock = async () =>{
+        if(!user) return;
+
+
+        const userDocRef = doc(db,"users",currentUser.id)
+
+        try{
+            await updateDoc(userDocRef,{
+                blocked:isReceiverBlocked ? arrayRemove(user.id) : arrayUnion(user.id),
+            })
+            changeBlock()
+        } catch (err){
+
+            console.log(err)
+        }
+    }
+
     return (
         <div className="flex-[1] detail">
         <div className="user px-[20px] py-[30px] flex flex-col items-center gap-[15px] border-[#dddddd35] border-b-[1px] solid">
-            <img src="./avatar.png" alt="" />
-            <h2>Hikaru Nakamura</h2>
+            <img src={user?.avatar || "./avatar.png"}alt="" />
+            <h2>{user?.username}</h2>
             <p>Lorem ipsum dolor sit amet.</p>
         </div>    
         <div className="info p-5 flex flex-col gap-[25px]">
@@ -81,8 +106,10 @@ const Detail = () => {
                 </div>
             </div>
 
-        <button className="px-[15px] py-[10px] text-white bg-[rgba(230,74,105,0.553)] border-none rounded-[5px] cursor-pointer hover:bg-red-900">Block User</button>
-        <button className="px-5 py-[10px] text-white bg-[rgba(35,77,51,0.81)] border-none rounded-[5px] cursor-pointer hover:bg-green-900">Logout</button>
+        <button className="px-[15px] py-[10px] text-white bg-[rgba(230,74,105,0.553)] border-none rounded-[5px] cursor-pointer hover:bg-red-900" onClick={handleBlock}>{
+            isCurrentUserBlocked? "You Are Blocked!":isReceiverBlocked? "User Blocked" : "Block User"
+        }</button>
+        <button className="px-5 py-[10px] text-white bg-[rgba(35,77,51,0.81)] border-none rounded-[5px] cursor-pointer hover:bg-green-900" onClick={()=>auth.signOut()}>Logout</button>
         </div>
         </div>
     );
